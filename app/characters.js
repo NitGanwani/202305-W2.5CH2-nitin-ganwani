bd20b6c44c9d619ef0b33cf6b32d6cabddc7f9b1
Nitin Ganwani <nitganwani@gmail.com>
2023-05-09 18:45:44 +0200

    Add characters.js

---
+++ app/characters.js
@@ -0,0 +1,14 @@
+export class Character {
+  name;
+  house;
+  age;
+  status;
+  series;
+  constructor(name, house, age, status) {
+    this.name = name;
+    this.house = house;
+    this.age = age;
+    this.status = status;
+    this.series = "GOT";
+  }
+}
